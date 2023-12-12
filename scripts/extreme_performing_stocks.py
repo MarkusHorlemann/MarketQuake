b41204aa4176817ea89fcf8a202789c5c225a293
Tim Pfeiffer <tim.pfeiffer@t-online.de>
2023-12-12 16:38:23 +0100

    Analysis of extreme performing stocks

--- scripts/extreme_performing_stocks.py
+++ scripts/extreme_performing_stocks.py
@@ -1,6 +1,9 @@
 from pyspark.sql import SparkSession
 from pyspark.sql import functions as f
 from pyspark.sql.window import Window as w
+import matplotlib.pyplot as plt
+import pandas as pd
+
 
 def find_extremes(csv_path):
     spark = SparkSession.builder.appName("StockMarketExtremes").getOrCreate()
@@ -8,22 +11,67 @@ def find_extremes(csv_path):
     # Load the CSV data
     df = spark.read.csv(csv_path, header=True, inferSchema=True)
 
-    # Define a window specification to rank stocks based on closing price
-    window_spec = w.partitionBy("stock_symbol").orderBy("date")
+    # Filter data for the specified date range (2020, week 1 to 2022, week 50)
+    filtered_data = df.filter((f.col("year") == 2020) & (f.col("week") >= 1) |
+                              (f.col("year") > 2020) & (f.col("year") < 2022) |
+                              (f.col("year") == 2022) & (f.col("week") <= 50))
 
-    # Calculate rank of each stock based on closing price
-    df_ranked = df.withColumn("rank", f.rank().over(window_spec))
+    # Calculate the average closing price for each stock
+    average_prices = filtered_data.groupBy("name").agg(f.avg("closing_price").alias("avg_closing_price"))
 
-    # Find the top-performing and bottom-performing stocks
-    top_performing_stocks = df_ranked.filter(f.col("rank") == 1)
-    bottom_performing_stocks = df_ranked.filter(f.col("rank") == f.max("rank").over(w.partitionBy()))
+    # Rank stocks based on average closing price
+    windowSpec = w.orderBy(f.desc("avg_closing_price"))
+    ranked_stocks = average_prices.withColumn("rank", f.rank().over(windowSpec))
 
-    # Calculate the correlation between individual stock performance and overall market trends
-    correlation_df = df.select("stock_symbol", "closing_price").groupBy("stock_symbol").agg(
-        f.corr("closing_price", "daily_deaths").alias("correlation")
-    )
+    # Identify top-performing and bottom-performing stocks
+    top_performing_stocks = ranked_stocks.filter(f.col("rank") == 1)
+    bottom_performing_stocks = ranked_stocks.filter(f.col("rank") == f.max("rank").over(w.partitionBy()))
+
+    """
+    # Calculate correlation between individual stock performance and overall market trends
+    correlation_data = filtered_data.join(average_prices, "name", "inner")
+    correlation = correlation_data.stat.corr("closing_price", "avg_closing_price")
+
+    # Display top-performing and bottom-performing stocks
+    print("Top-performing stocks:")
+    top_performing_stocks.show()
+
+    print("Bottom-performing stocks:")
+    bottom_performing_stocks.show()
+
+    print(f"Correlation between individual stock performance and overall market trends: {correlation}")
+
+    """
+
+    # Extract the names of top and bottom performing stocks
+    top_stock_names = top_performing_stocks.select("name").rdd.flatMap(lambda x: x).collect()
+    bottom_stock_names = bottom_performing_stocks.select("name").rdd.flatMap(lambda x: x).collect()
+
+    # Filter data for top and bottom performing stocks
+    top_stock_data = df.filter(f.col("name").isin(top_stock_names))
+    bottom_stock_data = df.filter(f.col("name").isin(bottom_stock_names))
+
+    # Convert Spark DataFrames to Pandas DataFrames for plotting
+    top_stock_pd = top_stock_data.toPandas()
+    bottom_stock_pd = bottom_stock_data.toPandas()
+
+    # Plotting
+    plt.figure(figsize=(12, 6))
+
+    for stock_name in top_stock_names:
+        stock_data = top_stock_pd[top_stock_pd["name"] == stock_name]
+        plt.plot(stock_data["week"], stock_data["closing_price"], label=f"{stock_name} (Top Performing)")
+
+    for stock_name in bottom_stock_names:
+        stock_data = bottom_stock_pd[bottom_stock_pd["name"] == stock_name]
+        plt.plot(stock_data["week"], stock_data["closing_price"], label=f"{stock_name} (Bottom Performing)")
+
+    plt.xlabel("Week")
+    plt.ylabel("Closing Price")
+    plt.title("Top and Bottom Performing Stocks Over Time")
+    plt.legend()
+    plt.grid(True)
+    plt.show()
 
     # Stop the Spark session
     spark.stop()
-
-    return top_performing_stocks, bottom_performing_stocks, correlation_df
