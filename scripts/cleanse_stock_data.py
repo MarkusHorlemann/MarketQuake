9c9215166e1faba9103e4dc83a8de8527008ad32
anastasiia.korzhylova <anastasiia.korzhylova@tu-dortmund.de>
2023-12-12 12:11:41 +0100

    Attempt to solve memory problem

--- scripts/cleanse_stock_data.py
+++ scripts/cleanse_stock_data.py
@@ -2,17 +2,22 @@
 calculating average prices and total volume for each week between January 2020 and December 2022.
 It ignores files with missing information for at least one week.'''
 
-import os
+import os, sys
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, weekofyear, year, avg, sum, to_date
+from pyspark.sql.functions import col, weekofyear, year, avg, sum, to_date, lit
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType, FloatType
 
 class MissingDataException(Exception):
     pass
 
-def clean_and_group_by_week(df, file_name):
+def clean_and_group_by_week(file_path):
     '''Groups stock prices and volume by week from January 2020 to December 2022.'''
+    # Read the CSV file
+    df = spark.read.csv(file_path, header=True, inferSchema=True)
+    file_name = file_path[file_path.rfind('/')+1 : -4] 
+
     # Add 'Name' column to DataFrame
-    df = df.withColumn("Name", file_name.replace('.csv', ''))
+    df = df.withColumn("Name", lit(file_name))
 
     # Convert Date string to PySpark date type
     df = df.withColumn("Date", to_date(col("Date"), "dd-MM-yyy"))
@@ -37,29 +42,36 @@ def clean_and_group_by_week(df, file_name):
     else:
         raise MissingDataException("DataFrame missing information for at least one week.")
 
+# Read folder name from argument
+if len(sys.argv) != 2:
+    raise Exception("No folder name passed!")
+elif len(sys.argv[1]) == 0:
+    raise Exception("Empty string passed as folder name!")
+folder = sys.argv[1]
+
 # Initialize Spark session
-spark = SparkSession.builder.appName("StockDataCleansing").getOrCreate()
+spark = SparkSession.builder \
+    .appName("StockDataCleansing") \
+    .config("spark.driver.memory", "4g") \
+    .config("spark.executor.memory", "4g") \
+    .getOrCreate()
 dataset_path = "gs://marketquake_data/stock_market_data"
 
-for folder in ['forbes2000', 'nasdaq', 'nyse', 'sp500']:
-    # Define DataFrame schema
-    final_df = spark.createDataFrame([], ["Name", "Year", "Week", "Volume", "Low", "High", "Open", "Close", "Adjusted Close"])
-    
-    # Load file names from GCS bucket
-    files = [line.strip() for line in os.popen(f'gsutil ls {dataset_path}/{folder}/*.csv')]
+# Load file names from GCS bucket
+files = [line.strip() for line in os.popen(f'gsutil ls {dataset_path}/{folder}/*.csv')]
+dfs = []
 
-    # Cleanse and merge dataframes
-    for file_path in files:
-        df = spark.read.csv(file_path, header=True)
-        file_name = file_path.replace(f"{dataset_path}/{folder}/", '')
-        try:
-            df = clean_and_group_by_week(df, file_name)
-            final_df = final_df.union(df)
-        except MissingDataException:
-            print(f"File {file_path} disregarded due to missing data.")
-    
-    # Write merged file to GCS
-    df.write.option("header","true").csv(f"{dataset_path}_clean/{folder}.csv")
+# Cleanse and merge dataframes
+for file_path in files:
+    try:
+        dfs.append(clean_and_group_by_week(file_path))
+        print(f"File {file_path} processed.")
+    except MissingDataException:
+        print(f"File {file_path} disregarded due to missing data.")
+
+# Merge and write to GCS
+final_df = dfs[0].union(*dfs[1:])
+final_df.write.option("header","true").csv(f"{dataset_path}_clean/{folder}.csv")
 
 # Stop Spark session
 spark.stop()
